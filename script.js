6afb86fb4a9e73662c364964deada1fc52b76867
Srilekha005 <bhashettysrilekha@gmail.com>
2024-11-26 16:26:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const signupForm = document.getElementById("signupForm");
+  
+//     signupForm.addEventListener("submit", function(event) {
+//       event.preventDefault();
+//       const emailInput = document.getElementById("email");
+//       const email = emailInput.value;
+  
+//       if (validateEmail(email)) {
+//         alert("Thank you for subscribing to our newsletter!");
+//         emailInput.value = "";
+//       } else {
+//         alert("Please enter a valid email address.");
+//       }
+//     });
+  
+//     function validateEmail(email) {
+//       const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$/;
+//       return emailPattern.test(email);
+//     }
+//   });
+  
+  
+  
